84d0b313a354aacb22b09ecb9e0b663dd74f2d39
MoisesPiguave <moi.emanuelps@gmail.com>
2025-04-24 15:32:37 +0100

    Update SolicitudCompra.java
    agrega todo del enum

--- src/main/java/SolicitudCompra.java
+++ src/main/java/SolicitudCompra.java
@@ -20,7 +20,7 @@ public class SolicitudCompra extends Documento {
     public void rechazar() {
         this.estado = EstadoSolicitud.RECHAZADA;
     }
-
+  // faltan dos estados amas que estan en el enum
     @Override
     public String toString() {
         return "SolicitudCompra{id=" + getId() + ", producto=" + producto + ", estado=" + estado+"}";}
