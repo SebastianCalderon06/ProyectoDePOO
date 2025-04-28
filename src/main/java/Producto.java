c41b1399848c8bf782079ad11d39873957d1235f
MoisesPiguave <moi.emanuelps@gmail.com>
2025-04-27 20:26:20 -0500

    Refactor(006) Getters y Setters actualizados

--- src/main/java/Producto.java
+++ src/main/java/Producto.java
@@ -21,16 +21,32 @@ public class Producto implements Calculable {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public double getPrecioUnitario() {
         return precioUnitario;
     }
 
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
     public int getCantidad() {
         return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
     }
 
     @Override
